ae909b34dfdb7e5023f29bf0945e383d58d7e17c
kyle <github@kylemiller.com>
2015-12-11 10:29:43 -0600

    #13 accordian menu WIP

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,20 +1,20 @@
-$(window).load(function() {
-  var menu = $("#atlas #treaties");
-
-  $.each(treaties, function( index, element ) {
-    console.log("creating menu item: "+element.key+", name: "+element.name)
-      var li = $(document.createElement("div"));
-      li.addClass("treaty_nav");
-      menu.append(li);
-      var link = $(document.createElement("a"));
-      li.append(link);
-      link = $(link);
-      link.attr("href","#"+element.key);
-      link.attr("id",element.key);
-      link.html(element.name);
-  });
-
-});
+// $(window).load(function() {
+//   var menu = $("#atlas #treaties");
+//
+//   $.each(treaties, function( index, element ) {
+//     console.log("creating menu item: "+element.key+", name: "+element.name)
+//       var li = $(document.createElement("div"));
+//       li.addClass("treaty_nav");
+//       menu.append(li);
+//       var link = $(document.createElement("a"));
+//       li.append(link);
+//       link = $(link);
+//       link.attr("href","#"+element.key);
+//       link.attr("id",element.key);
+//       link.html(element.name);
+//   });
+//
+// });
 
 
 $(function() {
