c5dc0256eed329fefd4c66dc1057dc3b2a1c9859
kyle <github@kylemiller.com>
2015-12-12 13:12:11 -0600

    #13 WIP working accordion

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,19 +1,25 @@
-$(window).load(function() {
-  var menu = $("#atlas #treaties");
-
-  var acc = $(document.createElement("ul"));
-  acc.addClass("accordion");
-  menu.append(acc);
-
-  create_sections(acc);
-
-});
+// $(window).load(function() {
+//   var menu = $("#atlas #treaties");
+//
+//   var acc = $(document.createElement("ul"));
+//   acc.addClass("accordion");
+//   menu.append(acc);
+//
+//   create_sections(acc);
+//
+//   // $('ul.accordion').accordion();
+//
+// });
 
 function create_sections(acc) {
 
     $.each(groups, function( index, group ) {
       var section = $(document.createElement("li"));
       acc.append(section);
+      if (index == 0) {
+        // section.addClass("current");
+      }
+      section.append(section_link);
       var section_link = $(document.createElement("a"));
       section.append(section_link);
       section_link.attr("href","#"+group.key);
@@ -25,7 +31,7 @@ function create_sections(acc) {
 
       $.each(treaties, function( index, element ) {
         if (is_elem_in_group(element, group.key)) {
-          section.append(add_links(element));
+          ul.append(add_links(element, group.key));
         }
       });
 
@@ -35,9 +41,11 @@ function create_sections(acc) {
 function add_links(element) {
   var li = $(document.createElement("li"));
   var link = $(document.createElement("a"));
+  var div = $(document.createElement("div"));
   li.append(link);
+  // li.append(div);
   link.attr("href","#"+element.key);
-   link.attr("id",element.key);
+  link.attr("id",element.key);
   link.html(element.name);
   return li;
 }
@@ -55,5 +63,18 @@ function is_elem_in_group(elem, type) {
 
 
 $(function() {
+  var menu = $("#atlas #treaties");
+
+  var acc = $(document.createElement("ul"));
+  acc.addClass("accordion");
+  menu.append(acc);
+
+  create_sections(acc);
+
+  // $('ul.accordion').accordion();
+  // setTimeout(function() {
+    // window.scrollTo(0, 0);
   $('ul.accordion').accordion();
+// }, 1000);
+  // $('a[href=#patents]').trigger('activate-node');
 });
