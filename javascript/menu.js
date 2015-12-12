3eea0660f385f416492dd11d066d0c9d80adedf1
kyle <github@kylemiller.com>
2015-12-12 02:26:29 -0600

    #13 wip dynamic accordion

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,20 +1,57 @@
-// $(window).load(function() {
-//   var menu = $("#atlas #treaties");
-//
-//   $.each(treaties, function( index, element ) {
-//     console.log("creating menu item: "+element.key+", name: "+element.name)
-//       var li = $(document.createElement("div"));
-//       li.addClass("treaty_nav");
-//       menu.append(li);
-//       var link = $(document.createElement("a"));
-//       li.append(link);
-//       link = $(link);
-//       link.attr("href","#"+element.key);
-//       link.attr("id",element.key);
-//       link.html(element.name);
-//   });
-//
-// });
+$(window).load(function() {
+  var menu = $("#atlas #treaties");
+
+  var acc = $(document.createElement("ul"));
+  acc.addClass("accordion");
+  menu.append(acc);
+
+  create_sections(acc);
+
+});
+
+function create_sections(acc) {
+
+    $.each(groups, function( index, group ) {
+      var section = $(document.createElement("li"));
+      acc.append(section);
+      var section_link = $(document.createElement("a"));
+      section.append(section_link);
+      section_link.attr("href","#"+group.key);
+      // link.attr("id",element.key);
+      section_link.html(group.name);
+
+      var ul = $(document.createElement("ul"));
+      section.append(ul);
+
+      $.each(treaties, function( index, element ) {
+        if (is_elem_in_group(element, group.key)) {
+          section.append(add_links(element));
+        }
+      });
+
+    });
+};
+
+function add_links(element) {
+  var li = $(document.createElement("li"));
+  var link = $(document.createElement("a"));
+  li.append(link);
+  link.attr("href","#"+element.key);
+   link.attr("id",element.key);
+  link.html(element.name);
+  return li;
+}
+
+function is_elem_in_group(elem, type) {
+   for(var index=0; index < elem.groups.length; index++) {
+    if (type === elem.groups[index]) {
+      console.log("is elem in group? "+elem.groups+" -- "+type);
+      return true;
+    }
+   }
+
+  return false;
+}
 
 
 $(function() {
